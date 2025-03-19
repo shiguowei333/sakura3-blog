ed56aec8c2a7180ac92a9a58dc51290bfb6edfd5
shi_guowei <shiguowei333@outlook.com>
2025-03-19 17:52:46 +0800

    重构静态路由，增加前端静态路由

--- frondend/src/router/modules/home.js
+++ frondend/src/router/modules/home.js
@@ -1,25 +1,37 @@
 const { VITE_HIDE_HOME } = import.meta.env;
 const Layout = () => import("@/layout/index.vue");
 
-export default {
-  path: "/",
-  name: "Home",
-  component: Layout,
-  redirect: "/welcome",
-  meta: {
-    icon: "ep:home-filled",
-    title: "首页",
-    rank: 0
-  },
-  children: [
-    {
-      path: "/welcome",
-      name: "Welcome",
-      component: () => import("@/views/welcome/index.vue"),
-      meta: {
-        title: "首页",
-        showLink: VITE_HIDE_HOME === "true" ? false : true
+export default [
+  {
+    path: "/",
+    name: "Home",
+    component: Layout,
+    redirect: "/welcome",
+    meta: {
+      icon: "ep:home-filled",
+      title: "首页",
+      rank: 0
+    },
+    children: [
+      {
+        path: "/welcome",
+        name: "Welcome",
+        component: () => import("@/views/welcome/index.vue"),
+        meta: {
+          title: "首页",
+          showLink: VITE_HIDE_HOME === "true" ? false : true
+        }
       }
+    ]
+  },
+  {
+    path: "/webinfo",
+    name: "webinfo",
+    component: () => import("@/views/blog/webinfo/index.vue"),
+    meta: {
+      icon: "ep:home-filled",
+      title: "网站信息",
+      showLink: true
     }
-  ]
-};
+  }
+];
