6cdb36f5ee83b0bf0b7a0865b1338a639295c495
shi_guowei <shiguowei333@outlook.com>
2025-04-02 17:05:54 +0800

    后端文章管理增删改查接口调整，主要是外键多对一和一对一关系的重构

--- backend/apps/article/serializers.py
+++ backend/apps/article/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Article
+from ..category.models import Category
 from ..tag.models import Tag
 
 
@@ -10,42 +11,47 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'tag_name']
 
-class QueryListArticleSerializer(serializers.ModelSerializer):
-    article_tags = TagSerializer(many=True)
-    class Meta:
-        model = Article
-        fields = ['id', 'article_title', 'article_category', 'article_tags', 'user', 'create_time','update_time']
 
 
 class ArticleSerializer(serializers.ModelSerializer):
 
-    id = serializers.UUIDField(read_only=True)
     user = serializers.SerializerMethodField()
     create_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
     update_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
-    article_tags = TagSerializer(many=True)
+    category_name = serializers.CharField(source='article_category.category_name', read_only=True)
+    article_tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True, write_only=True)
+    tags = TagSerializer(source='article_tags' ,many=True, read_only=True)
 
     def get_user(self, obj):
         return obj.user.nick_name
 
+
     class Meta:
         model = Article
-        fields = ['id', 'article_title', 'article_content', 'article_category', 'article_tags', 'user', 'create_time', 'update_time']
+        fields = ['id', 'article_title', 'article_category', 'category_name', 'article_tags', 'tags', 'user', 'create_time', 'update_time']
+        extra_kwargs = {
+            'article_category': {'write_only': True},
+        }
+
+
+class HandlerArticleSerializer(ArticleSerializer):
+
+    class Meta:
+        model = Article
+        fields = ['id', 'article_title', 'article_content', 'article_category', 'article_tags']
 
     def create(self, validated_data):
         user = self.context['request'].user
         tags = validated_data.pop('article_tags')
         article = Article.objects.create(user=user, **validated_data)
         for tag in tags:
-            article.article_tags.add(tag['id'])
+            article.article_tags.add(tag)
         article.save()
         return article
 
     def update(self, instance, validated_data):
         tags = validated_data.pop('article_tags')
-        instance.article_tags.clear()
-        for tag in tags:
-            instance.article_tags.add(tag['id'])
+        instance.article_tags.set(tags)
         for k, v in validated_data.items():
             setattr(instance, k, v)
         instance.save()
